0aca51c0712d86cd1bcaa4a6b38fdc1b8ea4cb08
Connor Svrcek <csvrcek@umich.edu>
2019-09-14 22:58:36 -0400

    Scraper

--- scraper/webscraper.py
+++ scraper/webscraper.py
@@ -2,26 +2,46 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-# TODO: add parameters into url based on category
-root_url = "https://projects.propublica.org/nonprofits/search?utf8=%E2%9C%93&q=&state%5Bid%5D=&ntee%5Bid%5D=4&c_code%5Bid%5D="
+a_c_h_url = "https://projects.propublica.org/nonprofits/search?utf8=%E2%9C%93&q=&state%5Bid%5D=&ntee%5Bid%5D=1&c_code%5Bid%5D="
+education_url = "https://projects.propublica.org/nonprofits/search?utf8=%E2%9C%93&q=&state%5Bid%5D=&ntee%5Bid%5D=2&c_code%5Bid%5D="
+e_a_url = "https://projects.propublica.org/nonprofits/search?utf8=%E2%9C%93&q=&state%5Bid%5D=&ntee%5Bid%5D=3&c_code%5Bid%5D="
+health_url = "https://projects.propublica.org/nonprofits/search?utf8=%E2%9C%93&q=&state%5Bid%5D=&ntee%5Bid%5D=4&c_code%5Bid%5D="
+h_s_url = "https://projects.propublica.org/nonprofits/search?utf8=%E2%9C%93&q=&state%5Bid%5D=&ntee%5Bid%5D=5&c_code%5Bid%5D="
+i_f_a_url = "https://projects.propublica.org/nonprofits/search?utf8=%E2%9C%93&q=&state%5Bid%5D=&ntee%5Bid%5D=6&c_code%5Bid%5D="
+p_s_b_url = "https://projects.propublica.org/nonprofits/search?utf8=%E2%9C%93&q=&state%5Bid%5D=&ntee%5Bid%5D=7&c_code%5Bid%5D="
+religion_url = "https://projects.propublica.org/nonprofits/search?utf8=%E2%9C%93&q=&state%5Bid%5D=&ntee%5Bid%5D=8&c_code%5Bid%5D="
+membership_benefit_url = "https://projects.propublica.org/nonprofits/search?utf8=%E2%9C%93&q=&state%5Bid%5D=&ntee%5Bid%5D=9&c_code%5Bid%5D="
+unknown_url = "https://projects.propublica.org/nonprofits/search?utf8=%E2%9C%93&q=&state%5Bid%5D=&ntee%5Bid%5D=10&c_code%5Bid%5D="
 
-response = requests.get(root_url)
+responses = []
 
-content = BeautifulSoup(response.content, "html.parser")
+responses.append(requests.get(a_c_h_url))
+responses.append(requests.get(education_url))
+responses.append(requests.get(e_a_url))
+responses.append(requests.get(health_url))
+responses.append(requests.get(h_s_url))
+responses.append(requests.get(i_f_a_url))
+responses.append(requests.get(p_s_b_url))
+responses.append(requests.get(religion_url))
+responses.append(requests.get(membership_benefit_url))
+responses.append(requests.get(unknown_url))
 
-counter = 0
+for response in responses:
+    content = BeautifulSoup(response.content, "html.parser")
 
-npo_list = []
+    counter = 0
 
-all_tds = content.findAll('td')
+    npo_list = []
 
-for index, t_d in enumerate(all_tds):
-    if len(t_d.text.splitlines()) == 3 and t_d.text.splitlines()[1].lstrip()[0] != '$':
-        non_profit_object = {
-            "title": all_tds[index - 1].find('a').text,
-            "location": t_d.text.splitlines()[1].lstrip()
-        }
-        npo_list.append(non_profit_object)
+    all_tds = content.findAll('td')
 
-with open('scraper/npo_data.json', 'w') as outfile:
-    json.dump(npo_list, outfile)
+    for index, t_d in enumerate(all_tds):
+        if len(t_d.text.splitlines()) == 3 and t_d.text.splitlines()[1].lstrip()[0] != '$':
+            non_profit_object = {
+                "title": all_tds[index - 1].find('a').text,
+                "location": t_d.text.splitlines()[1].lstrip()
+            }
+            npo_list.append(non_profit_object)
+
+    with open('scraper/npo_data.json', 'w') as outfile:
+        json.dump(npo_list, outfile)
